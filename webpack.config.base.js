1af810e30e869474966fbb4bb1b820383df39586
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-02 10:59:33 -0500

    Base dependencies for the client code

---
+++ webpack.config.base.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'client.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: []
+};
---
+++ webpack.config.development.js
@@ -0,0 +1,5 @@
+const baseConfig = require('./webpack.config.base.js');
+
+const config = Object.assign({}, baseConfig);
+
+module.exports = config;
