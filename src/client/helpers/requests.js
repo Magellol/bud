578d796d8bdf63fcf9f485e5e04ea5f67a12a597
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-03 11:53:28 -0500

    Basic user fetching and state updating.

---
+++ src/client/helpers/requests.js
@@ -0,0 +1,33 @@
+/**
+ * Request a endpoint.
+ * @param  {string} endpoint Url to query.
+ * @param  {object} options  Options to pass it to fetch().
+ * @return {Promise}         Promise that fullfils into a json response.
+ */
+function request(endpoint, options) {
+  return fetch(endpoint, options).then(response => response.json());
+}
+
+/**
+ * Shorthanded method to do a post requests to the api.
+ * @param  {string} endpoint  Url to query.
+ * @param  {Object} [body={}] body to send.
+ * @return {Promise}          Promise that fullfils into a json response.
+ */
+export function post(endpoint, body = {}) {
+  const options = Object.assign({}, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({ payload: body })
+  });
+
+  return request(endpoint, options);
+}
+
+export function get(endpoint) {
+  const options = { method: 'GET' };
+
+  return request(endpoint, options);
+}
--- src/client/routes/Home/components/Home/Home.js
+++ src/client/routes/Home/components/Home/Home.js
@@ -2,16 +2,40 @@ import React from 'react';
 import Logo from '../../../../components/Logo';
 import User from './components/User';
 import s from './Home.css';
+import { post, get } from '../../../../helpers/requests';
+
+const ENDPOINTS = {
+  users: '/api/users',
+  newUser: '/api/users/new'
+};
 
 const Home = React.createClass({
   getInitialState() {
     return {
-      username: ''
+      username: '',
+      users: []
     };
+  },
+
+  componentDidMount() {
+    return get(ENDPOINTS.users).then(response => this.setState({ users: response.data }));
   },
 
   handleCreateUser(event) {
     event.preventDefault();
+
+    const { username } = this.state;
+    return post(ENDPOINTS.newUser, { username })
+      .then((response) => {
+        if (response.status === 'success') {
+          return this.setState({
+            users: [...this.state.users, response.data]
+          });
+        }
+
+        // TODO Handle validation errors.
+      })
+      .catch(error => console.error(error)); // TODO proper handling.
   },
 
   handleUpdateUsername(event) {
@@ -26,11 +50,15 @@ const Home = React.createClass({
   },
 
   render() {
+    const { users } = this.state;
+
     return (
       <div className={s.wrapper}>
         <div className={s.logo}>
           <Logo />
         </div>
+
+        {users && this.renderUsers(users)}
 
         <form onSubmit={this.handleCreateUser}>
           <input
