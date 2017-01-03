f7eada5833f79134bbab8704f7225ea8caee20d2
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-03 11:01:58 -0500

    Some progress on the homescreen, added displaying users and the input to create new one

--- src/client/routes/Home/components/Home/Home.js
+++ src/client/routes/Home/components/Home/Home.js
@@ -1,17 +1,72 @@
 import React from 'react';
+import { connect, PromiseState } from 'react-refetch';
 import Logo from '../../../../components/Logo';
 import User from './components/User';
 import s from './Home.css';
 
-export default function Home() {
-  return (
-    <div className={s.wrapper}>
-      <div className={s.logo}>
-        <Logo />
-      </div>
+const Home = React.createClass({
+  propTypes: {
+    users: React.PropTypes.instanceOf(PromiseState)
+  },
 
-      <User username="Bear" first={true} />
-      <User username="Real" />
-    </div>
-  );
-}
+  getInitialState() {
+    return {
+      username: ''
+    };
+  },
+
+  handleCreateUser(event) {
+    event.preventDefault();
+
+    console.log(event);
+  },
+
+  handleUpdateUsername(event) {
+    const { value: username } = event.target;
+    this.setState({ username });
+  },
+
+  renderUsers(users) {
+    return users.map((user, index) => (
+      <User username={user.username} first={index === 0} key={user.id} />
+    ));
+  },
+
+  render() {
+    const { users } = this.props;
+
+    return (
+      <div className={s.wrapper}>
+        <div className={s.logo}>
+          <Logo />
+        </div>
+
+        {users.fulfilled && this.renderUsers(users.value.data)}
+
+        <form onSubmit={this.handleCreateUser}>
+          <input
+            className={s.input}
+            placeholder="Add"
+            value={this.state.username}
+            onChange={this.handleUpdateUsername}
+          />
+        </form>
+      </div>
+    );
+  }
+});
+
+export default connect(() => ({
+  users: '/api/users',
+  createUser(username) {
+    return {
+      createUserResponse: {
+        url: '/api/users/new',
+        method: 'POST',
+        body: JSON.stringify({
+          payload: { username }
+        })
+      }
+    };
+  }
+}))(Home);
