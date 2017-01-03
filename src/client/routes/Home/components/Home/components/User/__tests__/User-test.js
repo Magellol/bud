c050a73dc85b4bdb4308699e2a4b9621b63e0a92
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-03 14:20:04 -0500

    Fixed some linting issues.

--- src/client/routes/Home/components/Home/components/User/__tests__/User-test.js
+++ src/client/routes/Home/components/Home/components/User/__tests__/User-test.js
@@ -7,7 +7,6 @@ const user = shallow(<User username="bear" />);
 
 describe('Home/User', function () {
   it('Should render properly', function () {
-
     expect(user.length).to.be.equal(1);
     expect(user.text()).to.be.equal('bear');
     expect(user.instance().props.first).to.be.equal(false);
