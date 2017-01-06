e230b72465348683aa76a49e5e969f4f2560ccef
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-05 22:38:53 -0500

    Making a little scrollable area for mobile…it’s just experimenting.

--- src/client/components/AddExpenseForm/AddExpenseForm.js
+++ src/client/components/AddExpenseForm/AddExpenseForm.js
@@ -45,10 +45,13 @@ const AddExpenseForm = React.createClass({
         </div>
 
         <p className={s.label}>Goes in</p>
-        <div className={s.categoriesWrapper}>
-          <RadioButton label="Groceries" checked={this.state.category === "1"} onChange={this.handleCategoryChange} value="1" />
-          <RadioButton label="Video Games" checked={this.state.category === "2"} onChange={this.handleCategoryChange} value="2" />
-          <RadioButton label="Restaurants" checked={this.state.category === "3"} onChange={this.handleCategoryChange} value="3" />
+
+        <div className={s.scrollable}>
+          <div className={s.categoriesWrapper}>
+            <RadioButton label="Groceries" checked={this.state.category === "1"} onChange={this.handleCategoryChange} value="1" />
+            <RadioButton label="Video Games" checked={this.state.category === "2"} onChange={this.handleCategoryChange} value="2" />
+            <RadioButton label="Restaurants" checked={this.state.category === "3"} onChange={this.handleCategoryChange} value="3" />
+          </div>
         </div>
 
         <div className={s.submitWrapper}>
