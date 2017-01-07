7262b0429063e97af6ebdb9e1b844a1c1ffffc8c
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-07 14:56:01 -0500

    Allow expense names to be NULL.

--- src/server/models/Expense.js
+++ src/server/models/Expense.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     name: {
       type: DataTypes.STRING(100),
-      allowNull: false,
+      allowNull: true,
       validate: {
         isString(value) {
           if (typeof value !== 'string') {
