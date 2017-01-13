f23b7cb5ae0fd95852549724ecd0a0cf6bdf3b44
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-13 12:21:43 -0500

    Modifying the store for production testing

--- src/env-config/default.js
+++ src/env-config/default.js
@@ -18,7 +18,8 @@ module.exports = {
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
-    store: new Error('Session store must be defined'),
+    // store: new Error('Session store must be defined'),
+    store: false, // It's temporary, just to test a production env. We're going to use Redis.
     cookie: {
       maxAge: (2 * 86400000) // 2 days.
     }
