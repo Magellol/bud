87d62dd93b6ea1eb78bdeff5b50d2b34aa712286
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-12 19:01:40 -0500

    Start of a production build.

---
+++ webpack.config.production.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const baseConfig = require('./webpack.config.base');
+
+const config = Object.assign({}, baseConfig);
+
+// config.output.filename = 'bundle.[hash].js';
+
+config.module.loaders = [
+  ...config.module.loaders,
+  {
+    test: /\.css$/,
+    loader: ExtractTextPlugin.extract(
+      'style',
+      'css?modules',
+      'postcss'
+    )
+  }
+];
+
+config.plugins = [
+  ...config.plugins,
+  // new ExtractTextPlugin('main.[hash].css'),
+  new ExtractTextPlugin('main.css'),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false },
+    comments: false
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+];
+
+module.exports = config;
