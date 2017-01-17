7b7ee82451edaebc3b70b0595d6ae26b63262ca0
Thomas Lefebvre <lefebvre515@gmail.com>
2017-01-17 13:31:29 -0500

    Throwing a runtime error if Sentry isn’t set on production.

--- src/server/services/sentry.js
+++ src/server/services/sentry.js
@@ -1,6 +1,14 @@
 const config = require('config');
 const Raven = require('raven');
 
+if (process.env.NODE_ENV === 'production' && config.has('sentry.DSN') === false) {
+  throw new Error(
+    'Invalid Sentry DSN url string. ' +
+    'Production environments require having sentry set up through the SENTRY_DSN env variable. ' +
+    'If you don\'t have sentry yet, please visit: https://sentry.io — They have a free tier. It will keep you sane.'
+  );
+}
+
 if (config.debug === false) {
   Raven.disableConsoleAlerts();
 }
